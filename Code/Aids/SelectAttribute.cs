b11e2a72840da77a9bdd533079c00542b298fc00
Natalie Sophie Raassalu <naraas@taltech.ee>
2026-04-20 14:28:01 +0300

    SelectAttribute lisamine Aidsi

---
+++ Code/Aids/SelectAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Abc.Aids;
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class SelectAttribute(Type t, string displayProperty = null) : Attribute
+{
+    public Type EntityType { get; } = t;
+    public string DisplayProperty { get; } = string.IsNullOrWhiteSpace(displayProperty) ? "Name" : displayProperty;
+}
--- Code/Data/CountryCurrency.cs
+++ Code/Data/CountryCurrency.cs
@@ -1,11 +1,12 @@
-﻿using Abc.Data.Common;
+﻿using Abc.Aids;
+using Abc.Data.Common;
 
 namespace Abc.Data;
 
 public class CountryCurrency : DetailedEntity
 {
-    public Guid? CountryId { get; set; }
-    public Guid? CurrencyId { get; set; }
+    [Select(typeof(Country))]public Guid? CountryId { get; set; }
+    [Select(typeof(Currency))] public Guid? CurrencyId { get; set; }
     public Currency Currency { get; set; }
     public Country Country { get; set; }
 
