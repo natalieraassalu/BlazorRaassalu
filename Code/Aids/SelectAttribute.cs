c1958204be59c469f7b057fad911b9104671c218
Natalie Sophie Raassalu <naraas@taltech.ee>
2026-04-28 10:12:38 +0300

    SelectAttributeTests lisamine

--- Code/Aids/SelectAttribute.cs
+++ Code/Aids/SelectAttribute.cs
@@ -3,6 +3,7 @@
 [AttributeUsage(AttributeTargets.Property)]
 public sealed class SelectAttribute(Type t, string displayProperty = null) : Attribute
 {
+    public SelectAttribute():this(null,null) {}
     public Type EntityType { get; } = t;
     public string DisplayProperty { get; } = string.IsNullOrWhiteSpace(displayProperty) ? "Name" : displayProperty;
 }
--- Tests/Aids/GetRandomTests.cs
+++ Tests/Aids/GetRandomTests.cs
@@ -9,8 +9,17 @@ namespace Abc.Tests.Aids;
     private const sbyte min = sbyte.MinValue;
     private const sbyte max = sbyte.MaxValue;
 
-    [TestMethod] public void Int8Test() 
-        => Assert.AreNotEqual(GetRandom.Int8(min, max), GetRandom.Int8(min, max));
+    [TestMethod] public void Int8Test()
+    {
+        var x = GetRandom.Int8(min, max);
+        var y = GetRandom.Int8(min, max);
+        for (var i = 0; i < 10; i++)
+        {
+            if (x != y) break;
+            y = GetRandom.Int8(min, max);
+        }
+        Assert.AreNotEqual(x, y);
+    }
     [TestMethod] public void Int16Test()
         => Assert.AreNotEqual(GetRandom.Int16(min, max), GetRandom.Int16(min, max));
     [TestMethod] public void Int32Test() 
@@ -18,7 +27,16 @@ namespace Abc.Tests.Aids;
     [TestMethod] public void Int64Test()
         => Assert.AreNotEqual(GetRandom.Int64(min,max),GetRandom.Int64(min,max));
     [TestMethod] public void UInt8Test()
-        => Assert.AreNotEqual(GetRandom.UInt8(0,(byte) max), GetRandom.UInt8(0, (byte)max));
+    {
+        var x = GetRandom.UInt8(0, (byte)max);
+        var y = GetRandom.UInt8(0, (byte)max);
+        for (var i = 0; i < 10; i++)
+        {
+            if (x != y) break;
+            y = GetRandom.UInt8(0, (byte)max);
+        }
+        Assert.AreNotEqual(x, y);
+    }
     [TestMethod] public void UInt16Test()
         => Assert.AreNotEqual(GetRandom.UInt16(0,(ushort) max), GetRandom.UInt16(0, (ushort) max));
     [TestMethod] public void UInt32Test()
@@ -33,13 +51,9 @@ namespace Abc.Tests.Aids;
         => Assert.AreNotEqual(GetRandom.Decimal(min, max), GetRandom.Decimal(min, max));
     [TestMethod] public void StringTest()
         => Assert.AreNotEqual(GetRandom.String(0,(byte) max), GetRandom.String(0,(byte) max));
-
-    [TestMethod]
-    public void CharTest()
+    [TestMethod] public void CharTest()
         => Assert.AreNotEqual(GetRandom.Char((char)0, (char)max), GetRandom.Char((char)0, (char)max));
-
-    [TestMethod]
-    public void BoolTest()
+    [TestMethod] public void BoolTest()
     {
         var x= GetRandom.Bool();
         bool y = GetRandom.Bool();
@@ -50,27 +64,23 @@ namespace Abc.Tests.Aids;
         }
         Assert.AreNotEqual(x,y);
     }
-
-    [TestMethod]
-    public void DateTimeTest()
+    [TestMethod] public void DateTimeTest()
     {
         var minDate=DateTime.Now.AddYears(-50);
         var maxDate=DateTime.Now.AddYears(50);
         Assert.AreNotEqual(GetRandom.DateTime(minDate, maxDate), GetRandom.DateTime(minDate, maxDate));
     }
-
-    [TestMethod]
-    public void TimeSpanTest()
+    [TestMethod] public void TimeSpanTest()
     {
         var minSpan = TimeSpan.FromTicks(DateTime.Now.AddYears(-100).Ticks);
         var maxSpan = TimeSpan.FromTicks(DateTime.Now.AddYears(100).Ticks);
         Assert.AreNotEqual(GetRandom.TimeSpan(minSpan, maxSpan), GetRandom.TimeSpan(minSpan, maxSpan));
     }
-
+    [TestMethod] public void GuidTest()
+        => Assert.AreNotEqual(GetRandom.Guid(), GetRandom.Guid());
     private class testClass:NamedEntity{}
 
-    [TestMethod]
-    public void ObjectTest()
+    [TestMethod] public void ObjectTest()
     {
         var o1 = GetRandom.Object(typeof(testClass));
         var o2=GetRandom.Object(typeof(testClass));
